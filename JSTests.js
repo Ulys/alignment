fd61dfcf95e9d8f74ef2c29933c91bcfd24bb7ae
Anton Razumovskyy <anton.razumovskyy@gmail.com>
2013-10-26 12:47:35 +0300

    the tests are alive

--- JSTests.js
+++ JSTests.js
@@ -1,13 +1,11 @@
 test("getSequences()", function(){
 	var mySequences1 = new Object();
+	mySequences1.value = "";
 	var mySequences2 = new Object();
-	mySequences2.value = "";
+	mySequences2.value = "gagagc";
 	var mySequences3 = new Object();
-	mySequences3.value = "gagagc";
-	var mySequences4 = new Object();
-	mySequences4.value = "TROLOL";
-	equals(getSequences(mySequences1), 0, "Undefined object");
-	equals(getSequences(mySequences2), 0, "Empty string object");
-	equals(getSequences(mySequences3), "GAGAGC", "Small letters");
-	equals(getSequences(mySequences4), "TROLOL", "Capital letters");
+	mySequences3.value = "TROLOL";
+	equal(getSequences(mySequences1), 0, "Empty string object");
+	equal(getSequences(mySequences2), "GAGAGC", "Small letters");
+	equal(getSequences(mySequences3), "TROLOL", "Capital letters");
 })
